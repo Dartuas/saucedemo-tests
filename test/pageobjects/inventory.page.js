40134bd6ac4a2efd3853f98f689854bb63d03ff3
Dartuas <dartuas@gmail.com>
2026-05-27 02:03:54 +0300

    Final fixes: remove comments

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -1,13 +1,12 @@
 import Page from './page.js';
 
 class InventoryPage extends Page {
-    // Локатор бургер меню
+
     get burgerMenu() { return $('#react-burger-menu-btn'); }
     
-    // Локатор кнопки Logout
+
     get logoutButton() { return $('#logout_sidebar_link'); }
 
-    // Метод для логауту
     async logout() {
         await this.burgerMenu.click();
         await this.logoutButton.click();
